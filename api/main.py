49becc6d0eb61a878d56eb68b38fc83c67f8c306
austintian1-2025 <austin.tian1@gmail.com>
2025-12-10 18:44:11 -0500

    test

--- api/main.py
+++ api/main.py
@@ -20,7 +20,7 @@ FOOD_CHOICES = [
 
 # 2. App Initialization
 # This creates the FastAPI application instance.
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app = FastAPI()
 #templates = Jinja2Templates(directory="templates") # Assuming your templates are in a 'templates' folder
